f88b2f613f94cac18ec8f7befa91dc2e75276b18
kabanovskyd <dkabanov@broadinstitute.org>
2026-03-12 11:06:53 -0400

    Wrote automatic installer script

---
+++ backman-installer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if !command -v uv &> /dev/null; then
+    echo "Installing uv..."
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+else
+    echo "uv installed at $(which uv)"
+fi
+
+uv pip install -e .
+backman_path=$(find . -name "backman" -type f)
+export PATH=$PWD/.venv/bin:$PATH
+echo 'export PATH=$PWD/.venv/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
