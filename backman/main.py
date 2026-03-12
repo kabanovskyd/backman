b58aafa014f305bc95b6f2f42bd65543818bba36
kabanovskyd <dkabanov@broadinstitute.org>
2026-03-12 11:23:39 -0400

    Minor fixes

--- backman/main.py
+++ backman/main.py
@@ -165,7 +165,7 @@ def status(ctx):
     config = ctx.obj["config"]
     client = ctx.obj["client"]
     target_bucket = ctx.obj["target_bucket"]
-    print(target_bucket)
+    print(target_directories)
     for directory in target_directories.keys():
         target_subdirs = config['target_dirs'][directory]
         for subdir in target_subdirs:
