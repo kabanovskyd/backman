9cb329a3640a42299b06b45bf4cf706a7741e106
kabanovskyd <dkabanov@broadinstitute.org>
2026-03-16 12:05:51 -0400

    Disabled parallel downloads on backman-installer to avoid crashing the installation process

--- backman-installer.sh
+++ backman-installer.sh
@@ -8,7 +8,7 @@ else
 fi
 
 uv venv
-uv pip install -e .
+uv pip install -e .  --concurrent-downloads 1
 backman_path=$(find . -name "backman" -type f)
 export PATH=$PWD/.venv/bin:$PATH
 echo 'export PATH=$PWD/.venv/bin:$PATH' >> ~/.bashrc
